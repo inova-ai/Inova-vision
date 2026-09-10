aeb8d5919b7e5330aa7d0af29782ec8211ba0906
inova-ai <fakhruroji766@gmail.com>
2026-09-10 22:20:20 +0700

    Delete netlify/functions/api.js

--- netlify/functions/api.js
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import app from "../../server.js";
-
-export const handler = serverless(app, {
-  request: (req, event) => {
-    req.netlifyEvent = event;
-  }
-});
